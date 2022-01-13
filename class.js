c10f6dda4d02133f2fcebcf60dc80fdd046bdf01
fixed <fixed@debian-BULLSEYE-live-builder-AMD64>
2022-01-13 18:49:01 +0400

    [MV] move file

--- class.js
+++
@@ -1,9 +0,0 @@
-class Computer {
-  constructor(brand, proc) {
-    this.compBrand = brand;
-    this.compProc = proc;
-  }
-}
-
-myPc = new Computer("Lenovo", "octocore amd ryzen");
-document.getElementById("demo").innerHTML = "My computer is a " + "<strong>" + myPc.compBrand + "</strong>" + " <p><em>"+ myPc.compProc + " is her processor</em></p>";
